3790e5aff47417c4bc8db090e60cb4d96909fd7a
ViNi89 <nickvitsas@gmail.com>
2015-12-17 20:02:26 +0200

    initial commit

--- src/dtn.h
+++ src/dtn.h
@@ -1,6 +1,8 @@
 #ifndef __IOT_DTN__H
 #define __IOT_DTN__H
 
+#include "common.h"
+
 struct iotProducer {
 	int 	m_Len;
 	int 	m_EndFlag;
@@ -11,26 +13,17 @@ struct iotConsumer {
 	int 	m_Len;
 	int 	m_EndFlag;
 	char	m_Buff[256];
-} iotProducer;
+} iotConsumer;
 
 struct iotDataMule {
 	int 	m_Len;
 	int 	m_EndFlag;
 	char	m_Buff[256];
-} iotProducer;
+} iotDataMule;
 
-int 	iot_data_mule_create(struct iotPacket* p, char* msg);
-int 	iot_packet_create(struct iotPacket* p, char* msg);
+int 	iot_data_mule_create(struct iotDataMule* dm);
+int 	iot_producer_create(struct iotProducer* p);
+int 	iot_consumer_create(struct iotConsumer* c);
 
-
-struct iotPacket {
-	int 	m_Len;
-	int 	m_EndFlag;
-	char	m_Buff[256];
-} iotPacket;
-
-int 	iot_packet_create(struct iotPacket* p, char* msg);
-int 	iot_packet_create_json(char* json);
-void* 	iot_packet_create_serialized_json(char* json);
 
 #endif  // __IOT_DTN__H
--- src/packet.c
+++ src/packet.c
@@ -3,14 +3,20 @@
 // Mule data cache
 static struct iotCachedPacket	g_DataCache[CACHE_COUNT];
 
-int iot_cache_init(struct iotPacket* p) {
-	
+int iot_cache_init() {
+	iot_cache_clear();
+}
+
+int iot_cache_clear() {
+	memset(g_DataCache, 0, sizeof(g_DataCache));
 }
 
 int iot_cache_add(struct iotRecvPacket* p) {
 	
 }
 
-struct iotCachedPacket* 	iot_cache_find(struct iotPacket* p) {
+struct iotCachedPacket* iot_cache_find(struct iotPacket* p) {
 	
 }
+
+int iot_packet_serialize(struct iotSendPacket* p);
